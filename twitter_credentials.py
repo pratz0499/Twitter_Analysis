f0f1ed7bd18889600869c0b7627aed1a8f0a689f
pratz0499 <36686740+pratz0499@users.noreply.github.com>
2020-04-15 12:32:44 +0530

    Add files via upload

---
+++ twitter_credentials.py
@@ -0,0 +1,7 @@
+
+############variables that contain credentials to access twitter api############
+ACCESS_TOKEN='1241969435121840129-Qc0u4TYLMcKf9wSPVnlnHYLbkfsaM9'
+ACCESS_TOKEN_SECRET='eXL3Z57WN31Ry5nXyeYeqIrJep8ySiFYr9kQufU9JxH9W'
+CONSUMER_KEY='y4FgaNbBMswdb8aZm87DhpdbW'
+CONSUMER_SECRET='dskcizjJFAvH4fYJ4WIRvFd6zfLJKsIH0bCFZl2rcB6j8Fo0XT'
+    
